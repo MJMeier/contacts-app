dabf76031a949d44ff61ed5eb139feea8ec4b5f8
Michael Meier <mjmeier8@gmail.com>
2018-10-16 19:58:42 -0500

    added routes

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,2 +1,10 @@
 class Api::ContactsController < ApplicationController
+
+  
+  
+
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/contacts" => "contact#index"
+  get "/contacts/:id" => "contact#show"
+  post "/contacts" => "contact#create"
+  patch "/contacts/:id" => "contact#update"
+  delete "/contacts/:id" => "contact#destroy"
 end
