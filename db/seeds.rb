362046736019ec24614af7560317af20e6cc9ce3
Michael Meier <mjmeier8@gmail.com>
2018-10-16 19:46:54 -0500

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Contact.create(first_name: "Michael", last_name: "Meier", email: "meier8@gmail.com", phone_number: "8157621111")
+Contact.create(first_name: "Dylan", last_name: "Donnelly", email: "dylan@gmail.com", phone_number: "8157622222")
+Contact.create(first_name: "Jim", last_name: "Hartwig", email: "jim@gmail.com", phone_number: "8157623333")
